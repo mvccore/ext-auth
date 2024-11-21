557f5bb4ac6e36233b1ce89776856c593963dce0
Tom Flídr <tomflidr@gmail.com>
2024-11-21 16:00:56 +0100

    release v5.3

--- src/MvcCore/Ext/Auth.php
+++ src/MvcCore/Ext/Auth.php
@@ -23,7 +23,7 @@ class Auth extends \MvcCore\Ext\Auths\Basic {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.2.0';
+	const VERSION = '5.3.0';
 
 	/**
 	 * Full authentication module type with all features.
